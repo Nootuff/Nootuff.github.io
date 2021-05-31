f4d973ababb72b4045b7b3e971c7b7d98fecc7b0
Adam <36113439+Nootuff@users.noreply.github.com>
2021-05-31 14:26:30 +0100

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -1,1 +1,15 @@
-var test = "test"
+ const fadeTarget = document.querySelectorAll('.fadeTarget');
+
+        observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if(entry.intersectionRatio > 0) {
+                    entry.target.classList.add('fadeAnim');
+                }
+                else {
+                    entry.target.classList.add('none');
+                }
+            })
+        })
+        fadeTarget.forEach(image => {
+            observer.observe(image)
+        })
