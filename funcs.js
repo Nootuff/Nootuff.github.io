41bc3279c8b530fdd81f1bddef2ff0843c03dc14
Adam <36113439+Nootuff@users.noreply.github.com>
2021-06-06 09:16:50 +0100

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -1,15 +1,40 @@
- const fadeTarget = document.querySelectorAll('.fadeTarget');
+function animator(target, anim) {
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add(anim);
+      }
+      else {
+        entry.target.classList.add('none');
+      }
+    })
+  })
+  target.forEach(item => {
+    observer.observe(item)
+  })
+}
 
-        observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if(entry.intersectionRatio > 0) {
-                    entry.target.classList.add('fadeAnim');
-                }
-                else {
-                    entry.target.classList.add('none');
-                }
-            })
-        })
-        fadeTarget.forEach(image => {
-            observer.observe(image)
-        })
+const fadeTarget = document.querySelectorAll('.fadeTarget');
+animator(fadeTarget, 'fadeAnim');
+
+const glowTarget = document.querySelectorAll('#adam');
+animator(glowTarget, 'glowAnim');
+
+const slideSideTarget = document.querySelectorAll('.slideSideTarget');
+animator(slideSideTarget, "slideSideAnim");
+
+const proficiencyTable = document.querySelectorAll('.proficiency-table');
+animator(proficiencyTable, "openAnim");
+
+const slideUpTarget = document.querySelectorAll('.slideUpTarget');
+animator(slideUpTarget, "slideUpAnim");
+
+//Might have to make something custom to make the observber look at the proficiency table itself to trigger the animation on the rows, they should trigger in sequence bottom one first. Maybe modify the animator above. Do you even need to?
+
+/*
+const colLeft = document.querySelectorAll('.col-left');
+animator(colLeft, "colSlideLeftAnim");
+
+const colRight = document.querySelectorAll('.col-right');
+animator(colRight, "colSlideRightAnim");
+*/
