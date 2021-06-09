3262e3fca19e63393aac5319e80d13937d3b9be1
Adam <36113439+Nootuff@users.noreply.github.com>
2021-06-09 08:21:20 +0100

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -1,3 +1,15 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 function animator(target, anim) {
   observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -23,11 +35,19 @@ animator(glowTarget, 'glowAnim');
 const slideSideTarget = document.querySelectorAll('.slideSideTarget');
 animator(slideSideTarget, "slideSideAnim");
 
-const proficiencyTable = document.querySelectorAll('.proficiency-table');
-animator(proficiencyTable, "openAnim");
 
-const slideUpTarget = document.querySelectorAll('.slideUpTarget');
+const left = document.querySelectorAll('.left');
+animator(left, "slam-anim");
+
+
+const slideUpTarget = document.querySelectorAll(".slideUpTarget");
 animator(slideUpTarget, "slideUpAnim");
+
+const swingUpTarget = document.querySelectorAll(".swing-up-target");
+animator(swingUpTarget, "swing-up-anim");
+
+const scaleUpTarget = document.querySelectorAll("#scale-up-target");
+animator(scaleUpTarget, "scale-up-anim");
 
 //Might have to make something custom to make the observber look at the proficiency table itself to trigger the animation on the rows, they should trigger in sequence bottom one first. Maybe modify the animator above. Do you even need to?
 
